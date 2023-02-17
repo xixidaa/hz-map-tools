71f62e3d6526841101667dc0f59ab9f053f31934
wangning <575583692@qq.com>
2023-02-17 17:51:09 +0800

    refactor: geojson校验用json-editor实现

--- src/components/GeojsonValid.vue
+++ src/components/GeojsonValid.vue
@@ -8,10 +8,18 @@
 <template>
   <div class="geojson-valid-wrrap">
     <div class="geojson-textarea">
-      <el-input v-model="textarea" :rows="2" type="textarea" placeholder="请输入需要校验的geojson" />
+      <!-- <el-input
+        v-model="textarea"
+        :rows="2"
+        type="textarea"
+        placeholder="请输入需要校验的geojson"
+      /> -->
+      <div id="jsoneditor" style="width: 100%; height: 100%"></div>
     </div>
     <div class="valid-btn-box">
-      <el-button class="valid-btn" type="primary" @click="validateGeojson">开始校验</el-button>
+      <el-button class="valid-btn" type="primary" @click="validateGeojson"
+        >开始校验</el-button
+      >
       <el-button class="valid-btn" @click="textarea = ''">清空</el-button>
     </div>
   </div>
@@ -22,22 +30,75 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { ElMessage } from 'element-plus'
-import { useGeojsonValidate } from '../hooks/useGeojsonValidate'
+import { onBeforeUnmount, onMounted, ref } from "vue";
+import { ElMessage } from "element-plus";
+import { useGeojsonValidate } from "../hooks/useGeojsonValidate";
+import JsonEditor from "jsoneditor";
+import "jsoneditor/dist/jsoneditor.css";
 
-const { geoTest } = useGeojsonValidate()
-const textarea = ref('')
-const dialogVisible = ref(false)
-const errContent = ref([])
+const { geoTest } = useGeojsonValidate();
+const textarea = ref("");
+const dialogVisible = ref(false);
+const errContent = ref([]);
+let jsonEditor = null;
+const timer = ref(null);
 
-const validateGeojson = () => {
-  if (!textarea.value) {
-    return ElMessage.warning('请输入需要校验的geojson')
+onMounted(() => {
+  initJsonEditor();
+});
+
+const initJsonEditor = () => {
+  const container = document.getElementById("jsoneditor");
+  jsonEditor = new JsonEditor(container, {
+    mode: "code",
+    language: "zh-CN",
+    enableSort: false,
+    enableTransform: false,
+    navigationBar: false,
+    search: false,
+    allowSchemaSuggestions: true,
+    onBlur: editBlur,
+    onError: onError,
+  });
+  jsonEditor.focus();
+};
+
+const isError = ref(false);
+const editBlur = () => {
+  try {
+    const json = jsonEditor.get();
+    textarea.value = json;
+    isError.value = false;
+  } catch (e) {
+    isError.value = true;
   }
-  errContent.value = geoTest(textarea.value).errContent
-  dialogVisible.value = true
-}
+};
+
+const onError = () => {
+  isError.value = true;
+};
+
+const validateGeojson = async () => {
+  timer.value = setTimeout(() => {
+    if (isError.value) {
+      return ElMessage.warning("json有误,请手动修复或点击左上角工具修复后重试");
+    }
+    if (!textarea.value) {
+      return ElMessage.warning("请输入需要校验的geojson");
+    }
+    errContent.value = geoTest(textarea.value).errContent;
+    dialogVisible.value = true;
+  }, 0);
+};
+
+onBeforeUnmount(() => {
+  clearTimeout(timer.value);
+  timer.value = null;
+  if (jsonEditor) {
+    jsonEditor.destroy();
+    jsonEditor = null;
+  }
+});
 </script>
 
 <style lang="scss" scoped>
@@ -48,6 +109,7 @@ const validateGeojson = () => {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
   :deep(.geojson-textarea) {
     height: 92%;
   }
@@ -65,6 +127,16 @@ const validateGeojson = () => {
     align-items: center;
     .valid-btn {
       width: 100px;
+    }
+  }
+  :deep(.jsoneditor) {
+    border: 1px solid rgba(36, 39, 48, 0.7);
+  }
+  :deep(.jsoneditor-menu) {
+    background: rgba(36, 39, 48, 0.7);
+    border: rgba(36, 39, 48, 0.7);
+    & > a.jsoneditor-poweredBy {
+      display: none;
     }
   }
 }
